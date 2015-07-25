442e749d6dd23413b5deff936fc3a52501eef5d0
JoJo <jojo141a@hotmail.com>
2015-07-25 23:55:50 +0700

    add tab bar images and create view controller class

---
+++ MemeMe/MemeDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Surasak Adulprasertsuk on 7/25/15.
+//  Copyright (c) 2015 Surasak Adulprasertsuk. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Surasak Adulprasertsuk on 7/25/15.
+//  Copyright (c) 2015 Surasak Adulprasertsuk. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "Cell"
+
+class SentMemesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return 0
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UICollectionViewCell
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
